12b2a4edf7ad57cb5dbcc69c26c565035c3c854e
syuhei176 <syuhei176@gmail.com>
2022-11-07 10:41:06 +0900

    test: subReward

---
+++ test/PredyMath.t.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.7.6;
+
+import "forge-std/Test.sol";
+import "../src/libraries/PredyMath.sol";
+
+contract PredyMathTest is Test {
+    function testSubRewardReturnReward() public {
+        (int256 a, uint256 b) = PredyMath.subReward(100, 10);
+
+        assertEq(a, 90);
+        assertEq(b, 10);
+    }
+
+    function testSubRewardReturnCorrectedReward() public {
+        (int256 a, uint256 b) = PredyMath.subReward(100, 110);
+
+        assertEq(a, 0);
+        assertEq(b, 100);
+    }
+
+    function testSubRewardMarginIsZero() public {
+        (int256 a, uint256 b) = PredyMath.subReward(0, 10);
+
+        assertEq(a, 0);
+        assertEq(b, 0);
+    }
+
+    function testSubRewardMarginIsNegative(uint256 _rewardAmount) public {
+        uint256 rewardAmount = bound(_rewardAmount, 0, 1e20);
+
+        (int256 a, uint256 b) = PredyMath.subReward(-10, rewardAmount);
+
+        assertEq(a, -10);
+        assertEq(b, 0);
+    }
+}
